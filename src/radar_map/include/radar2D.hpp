4898291b528a5814d8374d6625407f25778d0a70
TingWeiWong <b06901160@ntu.edu.tw>
2023-04-16 00:04:46 -0400

    Add odom and radar sync. Need to trasnform pointclouds to current frame.

--- src/radar_map/include/radar2D.hpp
+++ src/radar_map/include/radar2D.hpp
@@ -2,25 +2,47 @@
 #include <tf2_ros/transform_broadcaster.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <nav_msgs/Odometry.h>
+#include <sensor_msgs/PointCloud2.h>
 #include <nav_msgs/Path.h>
 #include <geometry_msgs/PoseWithCovariance.h>
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <geometry_msgs/PoseArray.h>
+#include <sensor_msgs/LaserScan.h>
+#include <Eigen/Dense>
+#include <tf2_eigen/tf2_eigen.h>
+#include <deque>
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
+
+typedef message_filters::sync_policies::ExactTime<sensor_msgs::PointCloud2, nav_msgs::Odometry> MySyncPolicy;
+
 
 class Radar2D {
 public:
     Radar2D(ros::NodeHandle* nh);
 private:
-    ros::Subscriber odomSub_;
+
+    message_filters::Subscriber<sensor_msgs::PointCloud2> radarSub_;
+    message_filters::Subscriber<nav_msgs::Odometry> odomSub_;
+    message_filters::Synchronizer<MySyncPolicy> *sync;
+
     ros::Subscriber amclPoseSub_;
     ros::Publisher amclPathPub_, initPosePub_;
+
+    // PointCloud stacking
+    ros::Publisher densePointPub_;
+    std::deque<Eigen::Matrix4f> pose_mat_;
+    int pose_cap_; // How many poses to store in an array before stacking
+
     tf2_ros::TransformBroadcaster odom2BaseBr_;
     std::string parent_frame_; //odom
     std::string child_frame_; // base
     std::string global_frame_; // map
-    std::string odom_topic_, amcl_pose_topic_, amcl_path_topic_;
+    std::string odom_topic_, amcl_pose_topic_, amcl_path_topic_, radar_topic_;
     nav_msgs::Path amcl_path_;
 
     void odomCb(const nav_msgs::OdometryConstPtr& odom);
     void amclPoseCb(const geometry_msgs::PoseWithCovarianceStampedConstPtr& amcl_pose);
-    void setInitial();
+    Eigen::Matrix4f toMatrix(geometry_msgs::Pose pose);
+    void cb_radar(const sensor_msgs::PointCloud2ConstPtr pc_msg, const nav_msgs::OdometryConstPtr odom);
 };
--- src/radar_map/src/radar2D.cpp
+++ src/radar_map/src/radar2D.cpp
@@ -1,75 +1,44 @@
 #include "radar2D.hpp"
 
-
-
-Radar2D::Radar2D(ros::NodeHandle* nh) {
+Radar2D::Radar2D(ros::NodeHandle *nh)
+{
   odom_topic_ = "/dopplerIO";
   amcl_pose_topic_ = "/amcl_pose";
   amcl_path_topic_ = "/amcl_path";
+  radar_topic_ = "/radar_inlier";
   global_frame_ = "/map";
   parent_frame_ = "/odom";
   child_frame_ = "/base_link";
-  odomSub_ = nh->subscribe(odom_topic_, 1, &Radar2D::odomCb, this);
+
+  odomSub_.subscribe(*nh, odom_topic_, 20);
+  radarSub_.subscribe(*nh, radar_topic_, 20);
+  sync = new message_filters::Synchronizer<MySyncPolicy>(MySyncPolicy(20), radarSub_, odomSub_);
+  sync->registerCallback(boost::bind(&Radar2D::cb_radar, this, _1, _2));
+
   amclPoseSub_ = nh->subscribe(amcl_pose_topic_, 1, &Radar2D::amclPoseCb, this);
   amclPathPub_ = nh->advertise<nav_msgs::Path>(amcl_path_topic_, 1000);
   amcl_path_.header.frame_id = global_frame_;
+  pose_cap_ = 10;
   // initPosePub_ = nh->advertise<geometry_msgs::PoseWithCovarianceStamped>("/initialpose", 10);
   // setInitial();
 }
 
-void Radar2D::setInitial() {
-  /*
-    Double Bag initial pose
-    pose: 
-    position: 
-      x: 52.57637405395508
-      y: -3.3849635124206543
-      z: 0.0
-    orientation: 
-      x: 0.0
-      y: 0.0
-      z: 0.999865070137846
-      w: 0.016426853570914664
-    covariance: [0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.25,
-     0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 
-     0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 
-     0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.06853892326654787]
-  */
-  geometry_msgs::PoseWithCovarianceStamped pose;
-  pose.header.frame_id = global_frame_;
-  pose.header.stamp = ros::Time::now(); 
-
-  geometry_msgs::Point point;
-  geometry_msgs::Quaternion quat;
-  point.x = 52.57637405395508;
-  point.y = -3.3849635124206543;
-  point.z = 0.0;
-
-  quat.x = 0.0;
-  quat.y = 0.0;
-  quat.z = 0.999865070137846;
-  quat.w = 0.016426853570914664;
-
-  pose.pose.pose.position = point;
-  pose.pose.pose.orientation = quat;
-
-  pose.pose.covariance = {0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.25,
-     0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 
-     0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 
-     0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.06853892326654787};
+void Radar2D::cb_radar(const sensor_msgs::PointCloud2ConstPtr pc_msg, const nav_msgs::OdometryConstPtr odom)
+{
   
-  initPosePub_.publish(pose);
 }
 
-void Radar2D::amclPoseCb(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& amcl_pose) {
+void Radar2D::amclPoseCb(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr &amcl_pose)
+{
   geometry_msgs::PoseStamped pathPose;
   pathPose.pose = amcl_pose->pose.pose;
   amcl_path_.poses.push_back(pathPose);
   amclPathPub_.publish(amcl_path_);
 }
 
-
-void Radar2D::odomCb(const nav_msgs::OdometryConstPtr& odom) {
+void Radar2D::odomCb(const nav_msgs::OdometryConstPtr &odom)
+{
+  // Get odom->base_link from odometry message
   geometry_msgs::TransformStamped transformStamped;
   transformStamped.header.stamp = odom->header.stamp;
   transformStamped.header.frame_id = parent_frame_;
@@ -77,22 +46,40 @@ void Radar2D::odomCb(const nav_msgs::OdometryConstPtr& odom) {
   transformStamped.transform.translation.x = odom->pose.pose.position.x;
   transformStamped.transform.translation.y = odom->pose.pose.position.y;
   transformStamped.transform.translation.z = odom->pose.pose.position.z;
-
   transformStamped.transform.rotation.x = odom->pose.pose.orientation.x;
   transformStamped.transform.rotation.y = odom->pose.pose.orientation.y;
   transformStamped.transform.rotation.z = odom->pose.pose.orientation.z;
   transformStamped.transform.rotation.w = odom->pose.pose.orientation.w;
   odom2BaseBr_.sendTransform(transformStamped);
+
+  // Add newest odom to pose_arr_, check if it has exceeded limit
+  Eigen::Matrix4f m = toMatrix(odom->pose.pose);
+  pose_mat_.push_back(m);
+  // Check if pose_arr_.size exceeds capacity, if so perform queue.pop
+  if (pose_mat_.size() > pose_cap_)
+  {
+    pose_mat_.pop_front();
+    // Perform point cloud stacking
+  }
 }
 
-int main(int argc, char** argv )
+Eigen::Matrix4f Radar2D::toMatrix(geometry_msgs::Pose pose)
 {
-    ros::init(argc, argv, "Radar2D_node");
+  Eigen::Matrix4d m;
+  Eigen::Affine3d a3d;
+  Eigen::fromMsg(pose, a3d);
+  m = a3d.matrix();
+  return m.cast<float>();
+}
 
-    ros::NodeHandle nh("");
-    
-    Radar2D Radar2D(&nh);
-    
-    ros::spin();
-    return 0;
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "Radar2D_node");
+
+  ros::NodeHandle nh("");
+
+  Radar2D Radar2D(&nh);
+
+  ros::spin();
+  return 0;
 }
