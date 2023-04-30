b7703e46bb9ca5c5704a88c64d5eb1de6780d284
huangjuite <huangjuite@gmail.com>
2023-04-29 22:00:20 -0400

    quaternion diff

--- plot_results.py
+++ plot_results.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import interpolate
+from scipy.spatial.transform import Rotation as R
+from scipy.spatial.transform import Slerp
 
 lidar_pd = pd.read_csv("lidar_results.csv")
 radar_pd = pd.read_csv("radar_results.csv")
@@ -27,13 +29,11 @@ lidar_odom['stamp'] -= lidar_odom['stamp'][0]
 lidar_odom['std_x'] = np.sqrt(lidar_odom['var_x'].to_numpy())
 lidar_odom['std_y'] = np.sqrt(lidar_odom['var_y'].to_numpy())
 lidar_odom['std_theta'] = np.sqrt(lidar_odom['var_theta'].to_numpy())
-lidar_odom['theta'] += np.pi
 
 radar_odom['stamp'] -= radar_odom['stamp'][0]
 radar_odom['std_x'] = np.sqrt(radar_odom['var_x'].to_numpy())
 radar_odom['std_y'] = np.sqrt(radar_odom['var_y'].to_numpy())
 radar_odom['std_theta'] = np.sqrt(radar_odom['var_theta'].to_numpy())
-radar_odom['theta'] += np.pi
 
 figure, axis = plt.subplots(3, 1, figsize=(8, 8))
 axis[0].set_title("pose standard deviation")
@@ -63,8 +63,19 @@ lidar_x = f_l_x(radar_odom['stamp'])
 f_l_y = interpolate.interp1d(lidar_odom['stamp'], lidar_odom['y'])
 lidar_y = f_l_y(radar_odom['stamp'])
 
-f_l_theta = interpolate.interp1d(lidar_odom['stamp'], lidar_odom['theta'])
-lidar_theta = f_l_theta(radar_odom['stamp'])
+quat_lidar = np.asarray(
+    [lidar_odom['qx'], lidar_odom['qy'], lidar_odom['qz'], lidar_odom['qw']]).T
+quat_lidar = R.from_quat(quat_lidar)
+
+slerp = Slerp(lidar_odom['stamp'], quat_lidar)
+lidar_quat = slerp(radar_odom['stamp'])
+
+radar_quat = np.asarray(
+    [radar_odom['qx'], radar_odom['qy'], radar_odom['qz'], radar_odom['qw']]).T
+radar_quat = R.from_quat(radar_quat)
+
+diff = lidar_quat.inv() * radar_quat
+diff = diff.as_euler('xyz')[:, 2]
 
 pos_err = np.array([radar_odom['x']-lidar_x, radar_odom['y']-lidar_y]).T
 pos_err = np.linalg.norm(pos_err, axis=1)
@@ -73,13 +84,10 @@ figure, axis = plt.subplots(2, 1, figsize=(8, 6))
 axis[0].set_title("pose error")
 axis[0].plot(radar_odom['stamp'], pos_err)
 
-o_err = radar_odom['theta']-lidar_theta
-axis[1].plot(radar_odom['stamp'], o_err, color="orange")
+axis[1].plot(radar_odom['stamp'], diff, color="orange")
 
-axis[0].set(ylabel='position error')
-axis[1].set(ylabel='heading error', xlabel='time')
+axis[0].set(ylabel='position error (m)')
+axis[1].set(ylabel='heading error (rad)', xlabel='time')
+
 plt.savefig('pos_err', dpi=400, bbox_inches="tight")
-plt.show()
-
-plt.plot(lidar_odom['stamp'], lidar_odom['theta'])
 plt.show()
